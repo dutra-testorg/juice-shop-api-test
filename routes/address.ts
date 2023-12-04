83aeaf41165f5430010a799bfd6931eb803eb87f
Gustavo Dutra <gobannondutra@gmail.com>
2023-12-04 10:50:16 -0300

    Update address.ts

--- routes/address.ts
+++ routes/address.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+// adding some comment here to trigger sonar
+
 import { type Request, type Response } from 'express'
 import { AddressModel } from '../models/address'
 
